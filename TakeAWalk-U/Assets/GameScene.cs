bbd46c5819247a5944be04d7191f92d0a3abd0d8
awu <softengineerwu@gmail.com>
2017-03-27 10:20:57 +0800

    增加一个基本的unity版本。

---
+++ TakeAWalk-U/Assets/GameScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameObject btnObj = GameObject.Find("Btn_Start");//"Button"为你的Button的名称  
+        Button btn = btnObj.GetComponent<Button>();
+        btn.onClick.AddListener(delegate()
+        {
+            Debug.Log("switch to 1");
+            SceneManager.LoadScene("1");
+        });  
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ TakeAWalk-U/Assets/SwitchScene.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SwitchScene : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Invoke("Timer", 4.0f);
+    }
+
+    void Timer()
+    {
+        SceneManager.LoadScene("1");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
